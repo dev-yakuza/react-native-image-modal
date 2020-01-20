12adf3c819f5473622996ceca2982b0c0f0caa4d
dev-yakuza <dev.yakuza@gmail.com>
2020-01-20 09:42:25 +0900

    developing

--- Example/App.js
+++ Example/App.js
@@ -1,114 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {useState} from 'react';
+import {StyleSheet, View, Image, Dimensions} from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+const Style = StyleSheet.create({
+  Container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'flex-start',
   },
 });
+const App = ({onPress}) => {
+  const [imageSize, setImageSize] = useState({
+    width: 0,
+    height: 0,
+  });
+  // const image = Image.resolveAssetSource(url)
+  Image.getSize(
+    'https://cdn.pixabay.com/photo/2018/01/11/09/52/three-3075752_960_720.jpg',
+    (imageWidth, imageHeight) => {
+      const windowSize = Dimensions.get('window');
+      let width = 0;
+      let height = 0;
+      console.log('width: ', width);
+      console.log('height: ', height);
+      if (imageWidth > imageHeight) {
+        width = windowSize.width;
+        height = (imageHeight * windowSize.width) / imageWidth;
+      } else {
+        width = (imageWidth * windowSize.height) / imageHeight;
+        height = windowSize.height;
+      }
+      setImageSize({
+        width,
+        height,
+      });
+    },
+  );
+  return (
+    <View style={Style.Container}>
+      <Image
+        enableHorizontalBounce={true}
+        style={{
+          width: imageSize.width,
+          height: imageSize.height,
+        }}
+        source={{
+          uri:
+            'https://cdn.pixabay.com/photo/2018/01/11/09/52/three-3075752_960_720.jpg',
+        }}
+      />
+    </View>
+  );
+};
 
 export default App;
