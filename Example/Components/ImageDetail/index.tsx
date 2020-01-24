d413d41bb23f1236e4fd540b8a9a52b35895fb6e
dev-yakuza <dev.yakuza@gmail.com>
2020-01-24 17:39:08 +0900

    make image detail component

---
+++ Example/Components/ImageDetail/index.tsx
@@ -0,0 +1,529 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Image,
+  Dimensions,
+  Animated,
+  PanResponder,
+  ImageSourcePropType,
+} from 'react-native';
+
+interface Props {
+  source: ImageSourcePropType;
+  longPressTime?: number;
+  doubleClickInterval?: number;
+  enableDoubleClickZoom?: boolean;
+  panToMove?: boolean;
+  maxOverflow?: number;
+  enableSwipeDown?: boolean;
+  pinchToZoom?: boolean;
+  minScale?: number;
+  maxScale?: number;
+  clickDistance?: number;
+  swipeDownThreshold?: number;
+  enableCenterFocus?: boolean;
+  onLongPress?: () => void;
+  onDoubleClick?: () => void;
+  onMove?: (position?: IOnMove) => void;
+  onClick?: (eventParams: IOnClick) => void;
+  onSwipeDown?: () => void;
+  horizontalOuterRangeOffset?: (offsetX?: number) => void;
+  responderRelease?: (vx?: number, scale?: number) => void;
+}
+const App = ({
+  source,
+  longPressTime = 800,
+  doubleClickInterval = 175,
+  enableDoubleClickZoom = true,
+  panToMove = true,
+  maxOverflow = 100,
+  enableSwipeDown = false,
+  pinchToZoom = true,
+  minScale = 0.6,
+  maxScale = 10,
+  clickDistance = 10,
+  swipeDownThreshold = 230,
+  enableCenterFocus = true,
+  onLongPress,
+  onDoubleClick,
+  onMove,
+  onClick,
+  onSwipeDown,
+  horizontalOuterRangeOffset,
+  responderRelease,
+}: Props) => {
+  const windowSize = Dimensions.get('window');
+
+  const [animatedScale] = useState<Animated.Value>(new Animated.Value(1));
+  const [animatedPositionX] = useState<Animated.Value>(new Animated.Value(0));
+  const [animatedPositionY] = useState<Animated.Value>(new Animated.Value(0));
+
+  let lastPositionX: null | number = null;
+  let lastPositionY: null | number = null;
+  let zoomLastDistance: null | number = null;
+  let horizontalWholeCounter: number = 0;
+  let verticalWholeCounter: number = 0;
+  let isDoubleClick: boolean = false;
+  let isLongPress: boolean = false;
+  let isHorizontalWrap: boolean = false;
+  let centerDiffX: number = 0;
+  let centerDiffY: number = 0;
+  let singleClickTimeout: undefined | number = undefined;
+  let longPressTimeout: undefined | number = undefined;
+  let lastClickTime: number = 0;
+  let doubleClickX: number = 0;
+  let doubleClickY: number = 0;
+  let scale: number = 1;
+  let positionX: number = 0;
+  let positionY: number = 0;
+  let zoomCurrentDistance: number = 0;
+  let swipeDownOffset: number = 0;
+  let horizontalWholeOuterCounter: number = 0;
+
+  const imageDidMove = (type: string): void => {
+    if (onMove) {
+      onMove({
+        type,
+        positionX: positionX,
+        positionY: positionY,
+        scale: scale,
+        zoomCurrentDistance: zoomCurrentDistance,
+      });
+    }
+  };
+
+  const panResponderReleaseResolve = () => {
+    if (enableSwipeDown && swipeDownThreshold) {
+      if (swipeDownOffset > swipeDownThreshold) {
+        if (onSwipeDown) {
+          onSwipeDown();
+        }
+        return;
+      }
+    }
+
+    if (enableCenterFocus && scale < 1) {
+      scale = 1;
+      Animated.timing(animatedScale, {
+        toValue: scale,
+        duration: 100,
+      }).start();
+    }
+
+    if (windowSize.width * scale <= windowSize.width) {
+      positionX = 0;
+      Animated.timing(animatedPositionX, {
+        toValue: positionX,
+        duration: 100,
+      }).start();
+    }
+
+    if (windowSize.height * scale <= windowSize.height) {
+      positionY = 0;
+      Animated.timing(animatedPositionY, {
+        toValue: positionY,
+        duration: 100,
+      }).start();
+    }
+
+    if (windowSize.height * scale > windowSize.height) {
+      const verticalMax =
+        (windowSize.height * scale - windowSize.height) / 2 / scale;
+      if (positionY < -verticalMax) {
+        positionY = -verticalMax;
+      } else if (positionY > verticalMax) {
+        positionY = verticalMax;
+      }
+      Animated.timing(animatedPositionY, {
+        toValue: positionY,
+        duration: 100,
+      }).start();
+    }
+
+    if (windowSize.width * scale > windowSize.width) {
+      const horizontalMax =
+        (windowSize.width * scale - windowSize.width) / 2 / scale;
+      if (positionX < -horizontalMax) {
+        positionX = -horizontalMax;
+      } else if (positionX > horizontalMax) {
+        positionX = horizontalMax;
+      }
+      Animated.timing(animatedPositionX, {
+        toValue: positionX,
+        duration: 100,
+      }).start();
+    }
+
+    if (enableCenterFocus && scale === 1) {
+      positionX = 0;
+      positionY = 0;
+      Animated.timing(animatedPositionX, {
+        toValue: positionX,
+        duration: 100,
+      }).start();
+      Animated.timing(animatedPositionY, {
+        toValue: positionY,
+        duration: 100,
+      }).start();
+    }
+
+    horizontalWholeOuterCounter = 0;
+    swipeDownOffset = 0;
+
+    imageDidMove('onPanResponderRelease');
+  };
+
+  const imagePanResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderTerminationRequest: () => false,
+
+    onPanResponderGrant: evt => {
+      lastPositionX = null;
+      lastPositionY = null;
+      zoomLastDistance = null;
+      horizontalWholeCounter = 0;
+      verticalWholeCounter = 0;
+      isDoubleClick = false;
+      isLongPress = false;
+      isHorizontalWrap = false;
+
+      if (singleClickTimeout) {
+        clearTimeout(singleClickTimeout);
+      }
+
+      if (evt.nativeEvent.changedTouches.length > 1) {
+        const centerX =
+          (evt.nativeEvent.changedTouches[0].pageX +
+            evt.nativeEvent.changedTouches[1].pageX) /
+          2;
+        centerDiffX = centerX - windowSize.width / 2;
+
+        const centerY =
+          (evt.nativeEvent.changedTouches[0].pageY +
+            evt.nativeEvent.changedTouches[1].pageY) /
+          2;
+        centerDiffY = centerY - windowSize.height / 2;
+      }
+      if (longPressTimeout) {
+        clearTimeout(longPressTimeout);
+      }
+      longPressTimeout = setTimeout(() => {
+        isLongPress = true;
+        if (onLongPress) {
+          onLongPress();
+        }
+      }, longPressTime);
+
+      if (evt.nativeEvent.changedTouches.length <= 1) {
+        if (new Date().getTime() - lastClickTime < (doubleClickInterval || 0)) {
+          lastClickTime = 0;
+          if (onDoubleClick) {
+            onDoubleClick();
+          }
+
+          clearTimeout(longPressTimeout);
+
+          doubleClickX = evt.nativeEvent.changedTouches[0].pageX;
+          doubleClickY = evt.nativeEvent.changedTouches[0].pageY;
+
+          isDoubleClick = true;
+
+          if (enableDoubleClickZoom) {
+            if (scale > 1 || scale < 1) {
+              scale = 1;
+
+              positionX = 0;
+              positionY = 0;
+            } else {
+              const beforeScale = scale;
+              scale = 2;
+
+              const diffScale = scale - beforeScale;
+              positionX =
+                ((windowSize.width / 2 - doubleClickX) * diffScale) / scale;
+
+              positionY =
+                ((windowSize.height / 2 - doubleClickY) * diffScale) / scale;
+            }
+
+            imageDidMove('centerOn');
+
+            Animated.parallel([
+              Animated.timing(animatedScale, {
+                toValue: scale,
+                duration: 100,
+              }),
+              Animated.timing(animatedPositionX, {
+                toValue: positionX,
+                duration: 100,
+              }),
+              Animated.timing(animatedPositionY, {
+                toValue: positionY,
+                duration: 100,
+              }),
+            ]).start();
+          }
+        } else {
+          lastClickTime = new Date().getTime();
+        }
+      }
+    },
+    onPanResponderMove: (evt, gestureState) => {
+      if (isDoubleClick) {
+        return;
+      }
+
+      if (evt.nativeEvent.changedTouches.length <= 1) {
+        console.log('lastPositionX: ', lastPositionX);
+        let diffX = gestureState.dx - (lastPositionX || 0);
+        if (lastPositionX === null) {
+          diffX = 0;
+        }
+        let diffY = gestureState.dy - (lastPositionY || 0);
+        if (lastPositionY === null) {
+          diffY = 0;
+        }
+
+        lastPositionX = gestureState.dx;
+        lastPositionY = gestureState.dy;
+
+        horizontalWholeCounter += diffX;
+        verticalWholeCounter += diffY;
+
+        if (
+          (Math.abs(horizontalWholeCounter) > 5 ||
+            Math.abs(verticalWholeCounter) > 5) &&
+          longPressTimeout
+        ) {
+          clearTimeout(longPressTimeout);
+        }
+
+        if (panToMove) {
+          if (swipeDownOffset === 0) {
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+              isHorizontalWrap = true;
+            }
+
+            if (windowSize.width * scale > windowSize.width) {
+              if (horizontalWholeOuterCounter > 0) {
+                if (diffX < 0) {
+                  if (horizontalWholeOuterCounter > Math.abs(diffX)) {
+                    horizontalWholeOuterCounter += diffX;
+                    diffX = 0;
+                  } else {
+                    diffX += horizontalWholeOuterCounter;
+                    horizontalWholeOuterCounter = 0;
+                    if (horizontalOuterRangeOffset) {
+                      horizontalOuterRangeOffset(0);
+                    }
+                  }
+                } else {
+                  horizontalWholeOuterCounter += diffX;
+                }
+              } else if (horizontalWholeOuterCounter < 0) {
+                if (diffX > 0) {
+                  if (Math.abs(horizontalWholeOuterCounter) > diffX) {
+                    horizontalWholeOuterCounter += diffX;
+                    diffX = 0;
+                  } else {
+                    diffX += horizontalWholeOuterCounter;
+                    horizontalWholeOuterCounter = 0;
+                    if (horizontalOuterRangeOffset) {
+                      horizontalOuterRangeOffset(0);
+                    }
+                  }
+                } else {
+                  horizontalWholeOuterCounter += diffX;
+                }
+              }
+
+              positionX += diffX / scale;
+
+              const horizontalMax =
+                (windowSize.width * scale - windowSize.width) / 2 / scale;
+              if (positionX < -horizontalMax) {
+                positionX = -horizontalMax;
+                horizontalWholeOuterCounter += -1 / 1e10;
+              } else if (positionX > horizontalMax) {
+                positionX = horizontalMax;
+                horizontalWholeOuterCounter += 1 / 1e10;
+              }
+              animatedPositionX.setValue(positionX);
+            } else {
+              horizontalWholeOuterCounter += diffX;
+            }
+
+            if (horizontalWholeOuterCounter > (maxOverflow || 0)) {
+              horizontalWholeOuterCounter = maxOverflow || 0;
+            } else if (horizontalWholeOuterCounter < -(maxOverflow || 0)) {
+              horizontalWholeOuterCounter = -(maxOverflow || 0);
+            }
+
+            if (horizontalWholeOuterCounter !== 0) {
+              if (horizontalOuterRangeOffset) {
+                horizontalOuterRangeOffset(horizontalWholeOuterCounter);
+              }
+            }
+          }
+
+          if (windowSize.height * scale > windowSize.height) {
+            positionY += diffY / scale;
+            animatedPositionY.setValue(positionY);
+          } else {
+            if (enableSwipeDown && !isHorizontalWrap) {
+              swipeDownOffset += diffY;
+              if (swipeDownOffset > 0) {
+                positionY += diffY / scale;
+                animatedPositionY.setValue(positionY);
+
+                scale = scale - diffY / 1000;
+                animatedScale.setValue(scale);
+              }
+            }
+          }
+        }
+      } else {
+        if (longPressTimeout) {
+          clearTimeout(longPressTimeout);
+        }
+
+        if (pinchToZoom) {
+          let minX: number;
+          let maxX: number;
+          if (
+            evt.nativeEvent.changedTouches[0].locationX >
+            evt.nativeEvent.changedTouches[1].locationX
+          ) {
+            minX = evt.nativeEvent.changedTouches[1].pageX;
+            maxX = evt.nativeEvent.changedTouches[0].pageX;
+          } else {
+            minX = evt.nativeEvent.changedTouches[0].pageX;
+            maxX = evt.nativeEvent.changedTouches[1].pageX;
+          }
+
+          let minY: number;
+          let maxY: number;
+          if (
+            evt.nativeEvent.changedTouches[0].locationY >
+            evt.nativeEvent.changedTouches[1].locationY
+          ) {
+            minY = evt.nativeEvent.changedTouches[1].pageY;
+            maxY = evt.nativeEvent.changedTouches[0].pageY;
+          } else {
+            minY = evt.nativeEvent.changedTouches[0].pageY;
+            maxY = evt.nativeEvent.changedTouches[1].pageY;
+          }
+
+          const widthDistance = maxX - minX;
+          const heightDistance = maxY - minY;
+          const diagonalDistance = Math.sqrt(
+            widthDistance * widthDistance + heightDistance * heightDistance,
+          );
+          zoomCurrentDistance = Number(diagonalDistance.toFixed(1));
+
+          if (zoomLastDistance !== null) {
+            const distanceDiff = (zoomCurrentDistance - zoomLastDistance) / 200;
+            let zoom = scale + distanceDiff;
+
+            if (zoom < (minScale || 0)) {
+              zoom = minScale || 0;
+            }
+            if (zoom > (maxScale || 0)) {
+              zoom = maxScale || 0;
+            }
+
+            const beforeScale = scale;
+
+            scale = zoom;
+            animatedScale.setValue(scale);
+
+            const diffScale = scale - beforeScale;
+            positionX -= (centerDiffX * diffScale) / scale;
+            positionY -= (centerDiffY * diffScale) / scale;
+            animatedPositionX.setValue(positionX);
+            animatedPositionY.setValue(positionY);
+          }
+          zoomLastDistance = zoomCurrentDistance;
+        }
+      }
+
+      imageDidMove('onPanResponderMove');
+    },
+    onPanResponderRelease: (evt, gestureState) => {
+      if (longPressTimeout) {
+        clearTimeout(longPressTimeout);
+      }
+
+      if (isDoubleClick) {
+        return;
+      }
+
+      if (isLongPress) {
+        return;
+      }
+
+      const moveDistance = Math.sqrt(
+        gestureState.dx * gestureState.dx + gestureState.dy * gestureState.dy,
+      );
+      const {locationX, locationY, pageX, pageY} = evt.nativeEvent;
+
+      if (
+        evt.nativeEvent.changedTouches.length === 1 &&
+        moveDistance < (clickDistance || 0)
+      ) {
+        singleClickTimeout = setTimeout(() => {
+          if (onClick) {
+            onClick({locationX, locationY, pageX, pageY});
+          }
+        }, doubleClickInterval);
+      } else {
+        if (responderRelease) {
+          responderRelease(gestureState.vx, scale);
+        }
+
+        panResponderReleaseResolve();
+      }
+    },
+    onPanResponderTerminate: () => {},
+  });
+
+  const animateConf = {
+    transform: [
+      {
+        scale: animatedScale,
+      },
+      {
+        translateX: animatedPositionX,
+      },
+      {
+        translateY: animatedPositionY,
+      },
+    ],
+  };
+
+  return (
+    <View
+      style={{
+        backgroundColor: '#000000',
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+        width: windowSize.width,
+        height: windowSize.height,
+      }}
+      {...imagePanResponder!.panHandlers}>
+      <Animated.View style={animateConf} renderToHardwareTextureAndroid={true}>
+        <Image
+          resizeMode="contain"
+          style={{
+            width: windowSize.width,
+            height: windowSize.height,
+          }}
+          source={source}
+        />
+      </Animated.View>
+    </View>
+  );
+};
+
+export default App;
