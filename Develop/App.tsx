864dede99830cb5da155baaae95afd0d7740fc38
dev-yakuza <dev.yakuza@gmail.com>
2020-01-29 13:54:07 +0900

    make develop project

---
+++ Develop/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ImageModal from './ClassComponent/ImageModal';
+import {View} from 'react-native';
+
+interface Props {}
+const App = ({}: Props) => {
+  // // const imageSource = {
+  // //   uri:
+  // //     'https://cdn.pixabay.com/photo/2019/07/25/18/58/church-4363258_960_720.jpg',
+  // // };
+  // // const imageSource = {
+  // //   uri:
+  // //     'https://cdn.pixabay.com/photo/2018/01/11/09/52/three-3075752_960_720.jpg',
+  // // };
+  // const imageSource = require('./images/horizontal.jpg');
+  const imageSource = require('./images/vertical.jpg');
+
+  return (
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <ImageModal
+        swipeToDismiss={true}
+        resizeMode="contain"
+        style={{
+          width: 250,
+          height: 250,
+        }}
+        source={imageSource}
+        onLongPressOriginImage={() => console.log('onLongPressOriginImage')}
+        onTap={() => console.log('onTap')}
+        onDoubleTap={() => console.log('onDoubleTap')}
+        onLongPress={() => console.log('onLongPress')}
+        onOpen={() => console.log('onOpen')}
+        didOpen={() => console.log('didOpen')}
+        onMove={() => console.log('onMove')}
+        responderRelease={() => console.log('responderRelease')}
+        willClose={() => console.log('willClose')}
+        onClose={() => console.log('onClose')}
+      />
+    </View>
+  );
+};
+
+export default App;
