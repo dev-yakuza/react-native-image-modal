05ffefb4fff22cab5791cae3812553da0f5d76ee
dev-yakuza <dev.yakuza@gmail.com>
2024-05-19 21:38:30 +0900

    refactor: Update development example

--- Develop/App.tsx
+++ Develop/App.tsx
@@ -22,6 +22,14 @@ const Style = StyleSheet.create({
     paddingTop: 8,
     paddingBottom: 8,
   },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+    marginTop: 8,
+    marginHorizontal: 8,
+    borderBottomWidth: 1,
+  },
   text: {
     paddingHorizontal: 16,
     paddingVertical: 8,
@@ -40,6 +48,7 @@ const App = () => {
           onLayout={(event: LayoutChangeEvent) => {
             setImageWidth(event.nativeEvent.layout.width);
           }}>
+          <Text style={Style.heading}>Demo</Text>
           <Text style={Style.text}>
             Affronting discretion as do is announcing. Now months esteem oppose
             nearer enable too six. She numerous unlocked you perceive speedily.
@@ -55,10 +64,29 @@ const App = () => {
             raptures amounted occasion. One boy assure income spirit lovers set.
           </Text>
           <ImageModal
-            isTranslucent={false}
-            swipeToDismiss={false}
-            resizeMode="contain"
-            imageBackgroundColor="#000000"
+            isTranslucent
+            style={{
+              width: imageWidth,
+              height: 250,
+            }}
+            source={{
+              uri: 'https://cdn.pixabay.com/photo/2018/01/11/09/52/three-3075752_960_720.jpg',
+            }}
+          />
+          <Text style={Style.text}>
+            Game of as rest time eyes with of this it. Add was music merry any
+            truth since going. Happiness she ham but instantly put departure
+            propriety. She amiable all without say spirits shy clothes morning.
+            Frankness in extensive to belonging improving so certainty.
+            Resolution devonshire pianoforte assistance an he particular
+            middleton is of. Explain ten man uncivil engaged conduct. Am
+            likewise betrayed as declared absolute do. Taste oh spoke about no
+            solid of hills up shade. Occasion so bachelor humoured striking by
+            attended doubtful be it.
+          </Text>
+          <Text style={Style.heading}>Event</Text>
+          <Text style={Style.text}>You can see the log in the terminal.</Text>
+          <ImageModal
             style={{
               width: imageWidth,
               height: 250,
@@ -78,18 +106,15 @@ const App = () => {
             onClose={() => console.log('onClose')}
           />
           <Text style={Style.text}>
-            Game of as rest time eyes with of this it. Add was music merry any
-            truth since going. Happiness she ham but instantly put departure
-            propriety. She amiable all without say spirits shy clothes morning.
-            Frankness in extensive to belonging improving so certainty.
-            Resolution devonshire pianoforte assistance an he particular
-            middleton is of. Explain ten man uncivil engaged conduct. Am
-            likewise betrayed as declared absolute do. Taste oh spoke about no
-            solid of hills up shade. Occasion so bachelor humoured striking by
-            attended doubtful be it.
+            <Text>
+              - onLongPressOriginImage{'\n'}- onTap{'\n'}- onDoubleTap{'\n'}-
+              onLongPress{'\n'}- onOpen{'\n'}- didOpen{'\n'}- onMove{'\n'}-
+              responderRelease{'\n'}- willClose{'\n'}- onClose
+            </Text>
           </Text>
+          <Text style={Style.heading}>ResizeMode</Text>
+          <Text style={Style.text}>- Container</Text>
           <ImageModal
-            isTranslucent={false}
             resizeMode="contain"
             imageBackgroundColor="#000000"
             style={{
@@ -100,26 +125,48 @@ const App = () => {
               uri: 'https://cdn.pixabay.com/photo/2019/07/25/18/58/church-4363258_960_720.jpg',
             }}
           />
+          <Text style={Style.text}>- Cover</Text>
+          <ImageModal
+            resizeMode="cover"
+            imageBackgroundColor="#000000"
+            style={{
+              width: imageWidth,
+              height: 250,
+            }}
+            source={{
+              uri: 'https://cdn.pixabay.com/photo/2019/07/25/18/58/church-4363258_960_720.jpg',
+            }}
+          />
+          <Text style={Style.text}>- Stretch</Text>
+          <ImageModal
+            resizeMode="stretch"
+            imageBackgroundColor="#000000"
+            style={{
+              width: imageWidth,
+              height: 250,
+            }}
+            source={{
+              uri: 'https://cdn.pixabay.com/photo/2019/07/25/18/58/church-4363258_960_720.jpg',
+            }}
+          />
+          <Text style={Style.text}>- Center</Text>
+          <ImageModal
+            resizeMode="center"
+            imageBackgroundColor="#000000"
+            style={{
+              width: imageWidth,
+              height: 250,
+            }}
+            source={{
+              uri: 'https://cdn.pixabay.com/photo/2019/07/25/18/58/church-4363258_960_720.jpg',
+            }}
+          />
+          <Text style={Style.heading}>Translucent status bar</Text>
           <Text style={Style.text}>
-            Departure so attention pronounce satisfied daughters am. But shy
-            tedious pressed studied opinion entered windows off. Advantage
-            dependent suspicion convinced provision him yet. Timed balls match
-            at by rooms we. Fat not boy neat left had with past here call. Court
-            nay merit few nor party learn. Why our year her eyes know even how.
-            Mr immediate remaining conveying allowance do or.
-          </Text>
-          <Text style={Style.text}>
-            But why smiling man her imagine married. Chiefly can man her out
-            believe manners cottage colonel unknown. Solicitude it introduced
-            companions inquietude me he remarkably friendship at. My almost or
-            horses period. Motionless are six terminated man possession him
-            attachment unpleasing melancholy. Sir smile arose one share. No
-            abroad in easily relied an whence lovers temper by. Looked wisdom
-            common he an be giving length mr.
+            You can set true to isTranslucent to make status bar translucent.
           </Text>
           <ImageModal
-            isTranslucent={false}
-            resizeMode="contain"
+            isTranslucent
             imageBackgroundColor="#000000"
             style={{
               width: imageWidth,
@@ -127,15 +174,23 @@ const App = () => {
             }}
             source={require('./images/horizontal.jpg')}
           />
+          <Text style={Style.heading}>Image background color</Text>
           <Text style={Style.text}>
-            He do subjects prepared bachelor juvenile ye oh. He feelings
-            removing informed he as ignorant we prepared. Evening do forming
-            observe spirits is in. Country hearted be of justice sending. On so
-            they as with room cold ye. Be call four my went mean. Celebrated if
-            remarkably especially an. Going eat set she books found met aware.
+            You can set imageBackgroundColor to change the background color.
+          </Text>
+          <Text style={Style.text}>- imageBackgroundColor is not set</Text>
+          <ImageModal
+            resizeMode="contain"
+            style={{
+              width: imageWidth,
+              height: 250,
+            }}
+            source={require('./images/vertical.jpg')}
+          />
+          <Text style={Style.text}>
+            - imageBackgroundColor is set to #000000
           </Text>
           <ImageModal
-            isTranslucent={false}
             resizeMode="contain"
             imageBackgroundColor="#000000"
             style={{
@@ -144,14 +199,11 @@ const App = () => {
             }}
             source={require('./images/vertical.jpg')}
           />
+          <Text style={Style.heading}>Custom close button on Footer</Text>
           <Text style={Style.text}>
-            At distant inhabit amongst by. Appetite welcomed interest the
-            goodness boy not. Estimable education for disposing pronounce her.
-            John size good gay plan sent old roof own. Inquietude saw understood
-            his friendship frequently yet. Nature his marked ham wished.
+            You can set imageBackgroundColor to change the background color.
           </Text>
           <ImageModal
-            isTranslucent={false}
             resizeMode="contain"
             imageBackgroundColor="#000000"
             style={{
@@ -173,15 +225,55 @@ const App = () => {
               </TouchableOpacity>
             )}
           />
+          <Text style={Style.heading}>Custom close button on Header</Text>
           <Text style={Style.text}>
-            At distant inhabit amongst by. Appetite welcomed interest the
-            goodness boy not. Estimable education for disposing pronounce her.
-            John size good gay plan sent old roof own. Inquietude saw understood
-            his friendship frequently yet. Nature his marked ham wished.
+            You can set imageBackgroundColor to change the background color.
           </Text>
           <ImageModal
-            isTranslucent={false}
+            resizeMode="contain"
+            imageBackgroundColor="#000000"
+            style={{
+              width: imageWidth,
+              height: 250,
+            }}
+            source={require('./images/vertical.jpg')}
+            hideCloseButton
+            renderHeader={onClose => (
+              <TouchableOpacity
+                onPress={onClose}
+                style={{
+                  backgroundColor: '#FFFFFF',
+                  height: 100,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Text>CloseButton</Text>
+              </TouchableOpacity>
+            )}
+          />
+          <Text style={Style.heading}>Disable swipeToDismiss</Text>
+          <Text style={Style.text}>
+            You can set false to swipeToDismiss to disable swiping the modal to
+            dismiss.
+          </Text>
+          <ImageModal
             swipeToDismiss={false}
+            resizeMode="contain"
+            imageBackgroundColor="#000000"
+            style={{
+              width: imageWidth,
+              height: 250,
+            }}
+            source={{
+              uri: 'https://cdn.pixabay.com/photo/2018/01/11/09/52/three-3075752_960_720.jpg',
+            }}
+          />
+          <Text style={Style.heading}>Close modal programmatically</Text>
+          <Text style={Style.text}>
+            You can set false to swipeToDismiss to disable swiping the modal to
+            dismiss.
+          </Text>
+          <ImageModal
             resizeMode="contain"
             imageBackgroundColor="#000000"
             modalRef={element}
@@ -199,50 +291,9 @@ const App = () => {
               }, 3000);
             }}
           />
+          <Text style={Style.heading}>modalImageResizeMode</Text>
           <Text style={Style.text}>
-            Affronting discretion as do is announcing. Now months esteem oppose
-            nearer enable too six. She numerous unlocked you perceive speedily.
-            Affixed offense spirits or ye of offices between. Real on shot it
-            were four an as. Absolute bachelor rendered six nay you juvenile.
-            Vanity entire an chatty to.
-          </Text>
-          <Text style={Style.text}>
-            Prepared is me marianne pleasure likewise debating. Wonder an unable
-            except better stairs do ye admire. His and eat secure sex called
-            esteem praise. So moreover as speedily differed branched ignorant.
-            Tall are her knew poor now does then. Procured to contempt oh he
-            raptures amounted occasion. One boy assure income spirit lovers set.
-          </Text>
-          <ImageModal
-            disabled
-            resizeMode="contain"
-            imageBackgroundColor="#000000"
-            style={{
-              width: imageWidth,
-              height: 250,
-            }}
-            source={{
-              uri: 'https://cdn.pixabay.com/photo/2018/01/11/09/52/three-3075752_960_720.jpg',
-            }}
-          />
-          <Text style={Style.text}>
-            Affronting discretion as do is announcing. Now months esteem oppose
-            nearer enable too six. She numerous unlocked you perceive speedily.
-            Affixed offense spirits or ye of offices between. Real on shot it
-            were four an as. Absolute bachelor rendered six nay you juvenile.
-            Vanity entire an chatty to.
-          </Text>
-          <Text style={Style.text}>
-            Prepared is me marianne pleasure likewise debating. Wonder an unable
-            except better stairs do ye admire. His and eat secure sex called
-            esteem praise. So moreover as speedily differed branched ignorant.
-            Tall are her knew poor now does then. Procured to contempt oh he
-            raptures amounted occasion. One boy assure income spirit lovers set.
-          </Text>
-          <Text
-            style={{marginTop: 16, paddingHorizontal: 16, fontWeight: 'bold'}}>
-            resizeMode: "center"{'\n'}modalImageResizeMode: none{'\n'}
-            modalImageResizeMode will use resizeMode
+            - resizeMode: "center"{'\n'}- modalImageResizeMode: "contain"
           </Text>
           <ImageModal
             resizeMode="center"
@@ -253,23 +304,8 @@ const App = () => {
             }}
             source={require('./images/small.png')}
           />
-          <Text
-            style={{marginTop: 16, paddingHorizontal: 16, fontWeight: 'bold'}}>
-            resizeMode: "center"{'\n'}modalImageResizeMode: "contain"
-          </Text>
-          <ImageModal
-            resizeMode="center"
-            modalImageResizeMode="contain"
-            imageBackgroundColor="#000000"
-            style={{
-              width: 40,
-              height: 40,
-            }}
-            source={require('./images/small.png')}
-          />
-          <Text
-            style={{marginTop: 16, paddingHorizontal: 16, fontWeight: 'bold'}}>
-            resizeMode: "center"{'\n'}modalImageResizeMode: "cover"
+          <Text style={Style.text}>
+            - resizeMode: "center"{'\n'}- modalImageResizeMode: "cover"
           </Text>
           <ImageModal
             resizeMode="center"
@@ -282,19 +318,86 @@ const App = () => {
             source={require('./images/small.png')}
           />
           <Text style={Style.text}>
-            Affronting discretion as do is announcing. Now months esteem oppose
-            nearer enable too six. She numerous unlocked you perceive speedily.
-            Affixed offense spirits or ye of offices between. Real on shot it
-            were four an as. Absolute bachelor rendered six nay you juvenile.
-            Vanity entire an chatty to.
+            - resizeMode: "center"{'\n'}- modalImageResizeMode: "stretch"
           </Text>
+          <ImageModal
+            resizeMode="center"
+            modalImageResizeMode="stretch"
+            imageBackgroundColor="#000000"
+            style={{
+              width: 40,
+              height: 40,
+            }}
+            source={require('./images/small.png')}
+          />
           <Text style={Style.text}>
-            Prepared is me marianne pleasure likewise debating. Wonder an unable
-            except better stairs do ye admire. His and eat secure sex called
-            esteem praise. So moreover as speedily differed branched ignorant.
-            Tall are her knew poor now does then. Procured to contempt oh he
-            raptures amounted occasion. One boy assure income spirit lovers set.
+            - resizeMode: "center"{'\n'}- modalImageResizeMode: "center"
           </Text>
+          <ImageModal
+            resizeMode="center"
+            modalImageResizeMode="center"
+            imageBackgroundColor="#000000"
+            style={{
+              width: 40,
+              height: 40,
+            }}
+            source={require('./images/small.png')}
+          />
+          <Text style={Style.heading}>RTL</Text>
+          <ImageModal
+            isRTL
+            style={{
+              width: imageWidth,
+              height: 250,
+            }}
+            source={{
+              uri: 'https://cdn.pixabay.com/photo/2018/01/11/09/52/three-3075752_960_720.jpg',
+            }}
+          />
+          <Text style={Style.heading}>renderToHardwareTextureAndroid</Text>
+          <ImageModal
+            renderToHardwareTextureAndroid
+            style={{
+              width: imageWidth,
+              height: 250,
+            }}
+            source={{
+              uri: 'https://cdn.pixabay.com/photo/2018/01/11/09/52/three-3075752_960_720.jpg',
+            }}
+          />
+          <Text style={Style.heading}>overlayBackgroundColor</Text>
+          <ImageModal
+            overlayBackgroundColor="#FF0000"
+            style={{
+              width: imageWidth,
+              height: 250,
+            }}
+            source={{
+              uri: 'https://cdn.pixabay.com/photo/2018/01/11/09/52/three-3075752_960_720.jpg',
+            }}
+          />
+          <Text style={Style.heading}>disabled</Text>
+          <ImageModal
+            disabled
+            style={{
+              width: imageWidth,
+              height: 250,
+            }}
+            source={{
+              uri: 'https://cdn.pixabay.com/photo/2018/01/11/09/52/three-3075752_960_720.jpg',
+            }}
+          />
+          <Text style={Style.heading}>modalImageStyle</Text>
+          <ImageModal
+            style={{
+              width: imageWidth,
+              height: 250,
+            }}
+            modalImageStyle={{backgroundColor: '#00FF00'}}
+            source={{
+              uri: 'https://cdn.pixabay.com/photo/2018/01/11/09/52/three-3075752_960_720.jpg',
+            }}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
